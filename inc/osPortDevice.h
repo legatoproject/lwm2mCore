6f22b1d6a81015bb7ebfabe8d1cf6d79af7c65c4
Jean Thomas <jeanthomas@sierrawireless.com>
2017-03-16 09:23:27 +0100

    Add Object 3 to LWM2M monitoring
    LWM2M monitoring for the object 3 (Device) is added. This includes the
    following ressources:
    - /3/0/1: model number
    - /3/0/2: serial number
    - /3/0/3: firmware version
    - /3/0/9: current time.
    
    Resolves: LE-6340 LE-6217 LE-6211 LE-6216
    Change-Id: I725ed58b5f00a239a7e70c8d5d9648081e123789

--- inc/osPortDevice.h
+++ inc/osPortDevice.h
@@ -14,29 +14,24 @@
 
 //--------------------------------------------------------------------------------------------------
 /**
- *                  OBJECT 3: DEVICE
- */
-//--------------------------------------------------------------------------------------------------
-
-//--------------------------------------------------------------------------------------------------
-/**
  * Retrieve the device manufacturer
  * This API treatment needs to have a procedural treatment
  *
  * @return
- *      - LWM2MCORE_ERR_COMPLETED_OK if the treament succeeds
+ *      - LWM2MCORE_ERR_COMPLETED_OK if the treatment succeeds
  *      - LWM2MCORE_ERR_GENERAL_ERROR if the treatment fails
  *      - LWM2MCORE_ERR_INCORRECT_RANGE if the provided parameters (WRITE operation) is incorrect
  *      - LWM2MCORE_ERR_NOT_YET_IMPLEMENTED if the resource is not yet implemented
  *      - LWM2MCORE_ERR_OP_NOT_SUPPORTED  if the resource is not supported
  *      - LWM2MCORE_ERR_INVALID_ARG if a parameter is invalid in resource handler
  *      - LWM2MCORE_ERR_INVALID_STATE in case of invalid state to treat the resource handler
+ *      - LWM2MCORE_ERR_OVERFLOW in case of buffer overflow
  */
 //--------------------------------------------------------------------------------------------------
 lwm2mcore_sid_t os_portDeviceManufacturer
 (
-    char *bufferPtr,            ///< [INOUT] data buffer
-    size_t *lenPtr              ///< [INOUT] length of input buffer and length of the returned data
+    char*   bufferPtr,  ///< [INOUT] data buffer
+    size_t* lenPtr      ///< [INOUT] length of input buffer and length of the returned data
 );
 
 //--------------------------------------------------------------------------------------------------
@@ -45,19 +40,20 @@ lwm2mcore_sid_t os_portDeviceManufacturer
  * This API treatment needs to have a procedural treatment
  *
  * @return
- *      - LWM2MCORE_ERR_COMPLETED_OK if the treament succeeds
+ *      - LWM2MCORE_ERR_COMPLETED_OK if the treatment succeeds
  *      - LWM2MCORE_ERR_GENERAL_ERROR if the treatment fails
  *      - LWM2MCORE_ERR_INCORRECT_RANGE if the provided parameters (WRITE operation) is incorrect
  *      - LWM2MCORE_ERR_NOT_YET_IMPLEMENTED if the resource is not yet implemented
  *      - LWM2MCORE_ERR_OP_NOT_SUPPORTED  if the resource is not supported
  *      - LWM2MCORE_ERR_INVALID_ARG if a parameter is invalid in resource handler
  *      - LWM2MCORE_ERR_INVALID_STATE in case of invalid state to treat the resource handler
+ *      - LWM2MCORE_ERR_OVERFLOW in case of buffer overflow
  */
 //--------------------------------------------------------------------------------------------------
 lwm2mcore_sid_t os_portDeviceModelNumber
 (
-    char *bufferPtr,            ///< [INOUT] data buffer
-    size_t *lenPtr              ///< [INOUT] length of input buffer and length of the returned data
+    char*   bufferPtr,  ///< [INOUT] data buffer
+    size_t* lenPtr      ///< [INOUT] length of input buffer and length of the returned data
 );
 
 //--------------------------------------------------------------------------------------------------
@@ -66,19 +62,20 @@ lwm2mcore_sid_t os_portDeviceModelNumber
  * This API treatment needs to have a procedural treatment
  *
  * @return
- *      - LWM2MCORE_ERR_COMPLETED_OK if the treament succeeds
+ *      - LWM2MCORE_ERR_COMPLETED_OK if the treatment succeeds
  *      - LWM2MCORE_ERR_GENERAL_ERROR if the treatment fails
  *      - LWM2MCORE_ERR_INCORRECT_RANGE if the provided parameters (WRITE operation) is incorrect
  *      - LWM2MCORE_ERR_NOT_YET_IMPLEMENTED if the resource is not yet implemented
  *      - LWM2MCORE_ERR_OP_NOT_SUPPORTED  if the resource is not supported
  *      - LWM2MCORE_ERR_INVALID_ARG if a parameter is invalid in resource handler
  *      - LWM2MCORE_ERR_INVALID_STATE in case of invalid state to treat the resource handler
+ *      - LWM2MCORE_ERR_OVERFLOW in case of buffer overflow
  */
 //--------------------------------------------------------------------------------------------------
 lwm2mcore_sid_t os_portDeviceSerialNumber
 (
-    char *bufferPtr,            ///< [INOUT] data buffer
-    size_t *lenPtr              ///< [INOUT] length of input buffer and length of the returned data
+    char*   bufferPtr,  ///< [INOUT] data buffer
+    size_t* lenPtr      ///< [INOUT] length of input buffer and length of the returned data
 );
 
 //--------------------------------------------------------------------------------------------------
@@ -87,7 +84,29 @@ lwm2mcore_sid_t os_portDeviceSerialNumber
  * This API treatment needs to have a procedural treatment
  *
  * @return
- *      - LWM2MCORE_ERR_COMPLETED_OK if the treament succeeds
+ *      - LWM2MCORE_ERR_COMPLETED_OK if the treatment succeeds
+ *      - LWM2MCORE_ERR_GENERAL_ERROR if the treatment fails
+ *      - LWM2MCORE_ERR_INCORRECT_RANGE if the provided parameters (WRITE operation) is incorrect
+ *      - LWM2MCORE_ERR_NOT_YET_IMPLEMENTED if the resource is not yet implemented
+ *      - LWM2MCORE_ERR_OP_NOT_SUPPORTED  if the resource is not supported
+ *      - LWM2MCORE_ERR_INVALID_ARG if a parameter is invalid in resource handler
+ *      - LWM2MCORE_ERR_INVALID_STATE in case of invalid state to treat the resource handler
+ *      - LWM2MCORE_ERR_OVERFLOW in case of buffer overflow
+ */
+//--------------------------------------------------------------------------------------------------
+lwm2mcore_sid_t os_portDeviceFirmwareVersion
+(
+    char*   bufferPtr,  ///< [INOUT] data buffer
+    size_t* lenPtr      ///< [INOUT] length of input buffer and length of the returned data
+);
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Retrieve the battery level (percentage)
+ * This API treatment needs to have a procedural treatment
+ *
+ * @return
+ *      - LWM2MCORE_ERR_COMPLETED_OK if the treatment succeeds
  *      - LWM2MCORE_ERR_GENERAL_ERROR if the treatment fails
  *      - LWM2MCORE_ERR_INCORRECT_RANGE if the provided parameters (WRITE operation) is incorrect
  *      - LWM2MCORE_ERR_NOT_YET_IMPLEMENTED if the resource is not yet implemented
@@ -96,19 +115,18 @@ lwm2mcore_sid_t os_portDeviceSerialNumber
  *      - LWM2MCORE_ERR_INVALID_STATE in case of invalid state to treat the resource handler
  */
 //--------------------------------------------------------------------------------------------------
-lwm2mcore_sid_t os_portDeviceFirmwareVersion
+lwm2mcore_sid_t os_portDeviceBatteryLevel
 (
-    char *bufferPtr,            ///< [INOUT] data buffer
-    size_t *lenPtr              ///< [INOUT] length of input buffer and length of the returned data
+    uint8_t* valuePtr  ///< [INOUT] data buffer
 );
 
 //--------------------------------------------------------------------------------------------------
 /**
- * Retrieve the device time
+ * Retrieve the device time (UNIX time in seconds)
  * This API treatment needs to have a procedural treatment
  *
  * @return
- *      - LWM2MCORE_ERR_COMPLETED_OK if the treament succeeds
+ *      - LWM2MCORE_ERR_COMPLETED_OK if the treatment succeeds
  *      - LWM2MCORE_ERR_GENERAL_ERROR if the treatment fails
  *      - LWM2MCORE_ERR_INCORRECT_RANGE if the provided parameters (WRITE operation) is incorrect
  *      - LWM2MCORE_ERR_NOT_YET_IMPLEMENTED if the resource is not yet implemented
@@ -119,7 +137,7 @@ lwm2mcore_sid_t os_portDeviceFirmwareVersion
 //--------------------------------------------------------------------------------------------------
 lwm2mcore_sid_t os_portDeviceCurrentTime
 (
-    uint64_t* valuePtr          ///< [INOUT] data buffer
+    uint64_t* valuePtr  ///< [INOUT] data buffer
 );
 
 #endif /* __OS_PORTDEVICE_H__ */
